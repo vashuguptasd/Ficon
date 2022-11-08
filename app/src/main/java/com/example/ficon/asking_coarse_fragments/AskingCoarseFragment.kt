bc049c59e3e160d611ae971f02911ad8442428bc
Vashu gupta <vashuguptaag@gmail.com>
2022-11-09 00:00:23 +0530

    Recyclerview -> GridLayout + Fab

--- app/src/main/java/com/example/ficon/asking_coarse_fragments/AskingCoarseFragment.kt
+++ app/src/main/java/com/example/ficon/asking_coarse_fragments/AskingCoarseFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -29,24 +30,28 @@ class AskingCoarseFragment : Fragment() {
             val adapter = CoarseFragmentRecyclerViewAdapter()
 
             recyclerView.adapter = adapter
-            adapter.submitList(listOf(
-                CoarseDataClass("B.Sc","Batchelor of Science"),
-                CoarseDataClass("M.Sc","Master of Science"),
-                CoarseDataClass("B.A","Batchelor of Arts"),
-                CoarseDataClass("M.A","Master of Arts"),
-                CoarseDataClass("B.Com","Batchelor of Commerce"),
-                CoarseDataClass("M.Com","Master of Commerce"),
-                CoarseDataClass("B.C.A","Batchelor of Computer App.."),
-                CoarseDataClass("B.C.A","Master of Computer App..")
+            adapter.submitList(
+                listOf(
+                    CoarseDataClass("B.Sc", "Batchelor of Science"),
+                    CoarseDataClass("M.Sc", "Master of Science"),
+                    CoarseDataClass("B.A", "Batchelor of Arts"),
+                    CoarseDataClass("M.A", "Master of Arts"),
+                    CoarseDataClass("B.Com", "Batchelor of Commerce"),
+                    CoarseDataClass("M.Com", "Master of Commerce"),
+                    CoarseDataClass("B.C.A", "Batchelor of Computer App.."),
+                    CoarseDataClass("M.C.A", "Master of Computer App..")
 
-            ))
-            recyclerView.layoutManager = GridLayoutManager(application,1)
+                )
+            )
+            recyclerView.layoutManager = GridLayoutManager(application, 2)
             lifecycleOwner = lifecycleOwner
 
+            floatingActionButton.setOnClickListener {
+                Toast.makeText(application, "Fab Clicked", Toast.LENGTH_SHORT).show()
+            }
+
+            return binding.root
         }
-
-
-        return binding.root
     }
 
 }
